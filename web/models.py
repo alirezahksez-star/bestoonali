5e6c97717fe21f18aaea7abc71dfc43ff7fee2d1
alireza <alirezasez@gmail.com>
2026-02-09 10:46:54 +0330

    Ignore pycache and stop tracking compiled files

--- web/models.py
+++ web/models.py
@@ -7,11 +7,16 @@ class expence (models.Model):
     amount = models.BigIntegerField()
     
     user = models.ForeignKey(User , on_delete=models.CASCADE)
-
+    
+    def __str__(self):
+         return "{}-{}".format(self.date , self.amount)
+    
 class incom (models.Model):
     text = models.TextField(max_length=255)
     date = models.DateField()
     amount = models.BigIntegerField()
     
     user = models.ForeignKey(User , on_delete=models.CASCADE)
-    
+    def __str__(self):
+        return "{}-{}".format(self.date , self.amount)
+        
